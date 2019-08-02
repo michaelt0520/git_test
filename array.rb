6ca97982107189e8d614802bb0938cd6d2b73812
Hai Tuan <mictran205@gmail.com>
2019-08-02 13:58:10 +0700

    array

---
+++ array.rb
@@ -0,0 +1,28 @@
+arr = [1, 2, 5, -1, 5, -6]
+
+arr.shuffle
+
+arr.pop
+
+arr.shift
+
+arr.unshift -1
+
+arr.delete_at(0)
+
+arr.sort
+
+arr << -1
+
+arr.push
+
+arr.length
+
+arr.first
+
+arr.last
+
+arr.at(0)
+
+
+puts arr.first
